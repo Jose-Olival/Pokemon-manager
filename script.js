3e142049173cc0dbf9aeaeb911d0eadf06291601
Jose Olival <joseandresolival@gmail.com>
2025-08-28 15:00:05 -0400

    function get_pokemon_by_amount created

--- script.js
+++ script.js
@@ -7,3 +7,16 @@ const get_pokemon_data = async (id) => {
         console.log(err)
     }
 }
+
+const get_pokemon_by_amount = async (amount) => {
+    try{
+        let pokemons = []
+        for (let i = 1; i <= amount; i++){
+            let pokemon = await get_pokemon_data(i)
+            pokemons.push(pokemon)
+        }
+        return pokemons
+    }catch(err){
+        console.log(err)
+    }
+}
